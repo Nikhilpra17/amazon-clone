d32a245f41faa9941fa63b244e06b99c58a27b17
Nikhil Singh <nikhil.n.1@primathon.in>
2025-08-18 12:08:29 +0530

    Make the auth logic - mid way

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,49 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+//register user
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  const token = user.getJwtToken();
+
+  res.status(201).json({
+    success: true,
+    token,
+  });
+});
+
+//login user
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
